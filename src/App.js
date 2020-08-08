0566fc487871ab7a6582420b955ba854d4ae36bc
Gudaojingfeng <978298528@qq.com>
2020-08-08 19:17:04 +0800

    20881

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, {Component} from 'react';
+import { Button } from 'antd';
+
+// API
+import { Login } from "../src/api/account";
 
 class App  extends Component{
    constructor()
@@ -7,11 +11,23 @@ class App  extends Component{
 
    }
 
+   onclickTest(values)
+      {
+        Login(values).then(response => {  // resolves
+          console.log(response);
+        }).catch(error => {  // reject
+          console.log(error)
+        })
+        console.log('Received values of form: ', values);
+      };
+
+
     render()
     {
       return (
         <div className="App">
               Learn React
+              <Button    onClick={this.onclickTest.bind(this)} type="primary">Primary Button</Button>
         </div>
       )
     }
